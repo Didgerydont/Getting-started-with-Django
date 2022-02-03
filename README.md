47592f33c194730083225c24f4358d2d13b5e6da
Will Prendergast <prendygast@hotmail.com>
2022-02-03 21:57:04 +0000

    added template

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Getting-started-with-Django
 Refresher on Django from RealPython.com
 https://realpython.com/lessons/get-started-django-overview/
+
--- django-portfolio/portfolio/settings.py
+++ django-portfolio/portfolio/settings.py
@@ -63,7 +63,8 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'projects/templates'),
+                ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
